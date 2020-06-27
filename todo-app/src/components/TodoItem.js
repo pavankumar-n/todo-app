aa0b7c034d2135960a8bff4c0dea452ac9f8512f
Pavan Kumar <pavann@gain-insights.com>
2020-06-27 18:28:06 +0530

    ADD TodoItem Component

---
+++ todo-app/src/components/TodoItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+const classes = {
+    "Working on it": "in-progress",
+    "Waiting for review": "in-review",
+    "Done": "done",
+    "Stuck": "stuck",
+    "Urgent": "urgent",
+    "Low": "low",
+    "High": "high",
+    "Medium": "medium",
+    "": "very-low"
+}
+
+let TodoItem = ({ item }) => {
+
+    return (
+        <tr key={item} className="todo-item">
+            <td className="text-left pl-2">{item.label}</td>
+            <td>
+                <i className="fa fa-comment-o f-18 fa-icon-light" aria-hidden="true"></i>
+            </td>
+            <td>
+                <i className="fa fa-user-circle f-18 fa-icon-light" aria-hidden="true"></i>
+            </td>
+            <td className={classes[item.status]}>{item.status}</td>
+            <td className={item.status === "Done" ? "line-through" : ""}>{item.dueDate}</td>
+            <td className={classes[item.priority]}>{item.priority}</td>
+            <td></td>
+        </tr>
+    )
+}
+
+export default TodoItem;
